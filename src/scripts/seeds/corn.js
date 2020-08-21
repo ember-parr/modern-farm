c06e984603b38c5197fa60ec7b36cb5398f54b96
Ember Parr <ember-devops@gmail.com>
2020-08-21 10:33:50 -0500

    changed corn array

--- src/scripts/seeds/corn.js
+++ src/scripts/seeds/corn.js
@@ -1,15 +1,15 @@
 export const createCorn = () => {
-    const cornOne = {
-        type: "Corn",
-        height: 180,
-        output: 6
-    },
-    const cornTwo = {
-        type: "Corn",
-        height: 180,
-        output: 6
-    }
-
-    return [cornOne, cornTwo]
-    
+    const corn = [
+        {
+            type: "Corn",
+            height: 180,
+            output: 6
+        },
+        const cornTwo = {
+            type: "Corn",
+            height: 180,
+            output: 6
+        }
+    ]
+    return corn    
 }
